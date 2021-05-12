13ad84c8304a732d29f269713b3726150fa0f67e
justfoolingaround <gamerkritan+github@gmail.com>
2021-05-12 20:55:35 +0545

    [Twist] fix: Zero error, offset by 1

--- core/providers/twistmoe/__init__.py
+++ core/providers/twistmoe/__init__.py
@@ -6,5 +6,5 @@ def fetcher(session, url, check):
     
     anime_name = re.match(r'^(?:https?://)?(?:\S+\.)?twist\.moe/a/([^?&/]+)', url).group(1)
     for index, data in enumerate(get_twistmoe_anime_uri(session, anime_name), 1):
-        if check(index + 1):
+        if check(index):
             yield [{'quality': 'unknown', 'stream_url': data.get('stream_url'), 'headers': {'referer': 'https://twist.moe'}}]
