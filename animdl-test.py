64ab8f86facce7d70f723c0491c4b4c7ffb051d2
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-05-24 14:19:10 +0545

    Update: last-curated-test.json

--- animdl-test.py
+++ animdl-test.py
@@ -4,8 +4,14 @@ An effective benchmark on the providers.
 Tries to get a single stream link across each provider.
 """
 
+current_version = "20210524-1"
+
 import requests
 from core import Associator
+
+SHOW_FULL_URLS = False
+LAST_CURATED_TEST = requests.get("https://raw.githubusercontent.com/justfoolingaround/animdl/master/last-curated-test.json").json()
+PROVIDERS = LAST_CURATED_TEST.get('status', {})
 
 def get_animepahe_onepiece():
     """
@@ -23,9 +29,30 @@ SITE_BASED = {
     'Twist': 'https://twist.moe/a/one-piece',
 }
 
+RESULTS = {}
+
 for anime, anime_url in SITE_BASED.items():
     try:
         episode = [*Associator(anime_url).fetch_appropriate(start=1, end=1)].pop(0)        
-        print("Fetch from \x1b[34m{}\x1b[39m has been deemed \x1b[32msuccessful\x1b[39m: {}".format(anime, episode.urls))
+        print("Fetch from \x1b[36m{}\x1b[39m has been deemed \x1b[32msuccessful\x1b[39m{}".format(anime, (": %s" % episode.urls) if SHOW_FULL_URLS else ''))
+        RESULTS.update({anime: True})
     except Exception as e:
-        print("Fetch from \x1b[34m{}\x1b[39m has \x1b[31mfailed\x1b[39m, cause of failure: {}".format(anime, e))
+        print("Fetch from \x1b[36m{}\x1b[39m has \x1b[31mfailed\x1b[39m, cause of failure: {}".format(anime, e))
+        RESULTS.update({anime: False})
+        
+print("\x1b[33mComparing the test results to the last curated test by AnimDL's developers.\x1b[39m")
+
+for anime, anime_result in RESULTS.items():
+    if anime_result:
+        if (anime_result != PROVIDERS.get('anime', False)):
+            print("\x1b[36m{}\x1b[39m should not be working but is working now; you can raise an issue for requesting an update on this.")
+        else:
+            print("\x1b[36m{}\x1b[39m's status has matched with the curated test.")
+    else:
+        if (anime_result != PROVIDERS.get('anime', False)):  
+            print("\x1b[36m{}\x1b[39m is not working as intended; raise an issue immediately to get this fixed!")
+        else:
+            print("\x1b[36m{}\x1b[39m is not working currently, the developers are aware of this and the issue will get fixed soon.")
+            
+if LAST_CURATED_TEST.get('current_version') != current_version:
+    print("\x1b[31m{}\x1b[39m".format('The above results are subject to be severely outdated. Please update the code by performing a git pull or re-downloading the repository.'))
