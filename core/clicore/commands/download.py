3f08cebd5ec323a6c394b94cf1c8efc5980f889d
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-05-26 10:20:20 +0545

    Fix: Forgotten 'continue'

--- core/clicore/commands/download.py
+++ core/clicore/commands/download.py
@@ -67,10 +67,11 @@ def animdl_download(query, start, end, title, filler_list, offset, filler, mixed
         if raw_episodes:
             content_title += " - %s" % raw_episodes[c - 1].title
         
-        valid_urls = [_ for _ in stream_urls if not aed(_.get('stream_url', '')) in ['m3u8', 'm3u8']]
+        valid_urls = [_ for _ in stream_urls if not aed(_.get('stream_url', '')) in ['m3u8', 'm3u']]
         
         if not valid_urls:
             ts("Failed to download '%s' due to lack of downloadable stream urls. (Possible that m3u8 streams were only available in the site.)" % content_title)
+            continue
         
         content = valid_urls[0]
         url_download(content.get('stream_url'), base / Path('%s.%s' % (content_title, aed(content.get('stream_url')))), lambda r: tqdm(desc=content_title, total=r, unit='B', unit_scale=True, unit_divisor=1024), content.get('headers', {}))
