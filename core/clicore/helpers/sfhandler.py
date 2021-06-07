83f88abe060c8d4155067fd7412bf1317bdcfb9b
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-07 08:37:46 +0545

    Fix: Session search str -> dict

--- core/clicore/helpers/sfhandler.py
+++ core/clicore/helpers/sfhandler.py
@@ -46,7 +46,7 @@ def generate_stream_arguments(session_dict):
 def search_identifiers(session_file, identifer):
     for session in load_sessions(session_file):
         if identifer.lower() in session.get('identifier', '').lower():
-            yield session.get('identifier', '').lower()
+            yield session
         
 def get_most_recent_session(session_file):
     sessions = load_sessions(session_file)
