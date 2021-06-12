d8c810c723d54df8012baa348e2a6a8590afff1a
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-12 13:12:48 +0545

    Optimization: API calls [2/fetch -> 1/fetch, min]

--- core/providers/anime1/__init__.py
+++ core/providers/anime1/__init__.py
@@ -12,22 +12,15 @@ from requests.exceptions import Timeout
 EPISODE_RE = construct_site_based_regex(ANIME1, extra_regex=r'/watch/([^?&/]+)/episode-\d+')
 STREAM_EXTRACTOR_RE = re.compile(r'(?<=file: ")(?:(?!\").)+')
 
-is_healthy = lambda session, url: session.get(url, stream=True, verify=False, timeout=3).ok
+is_healthy = lambda session, url: session.head(url, stream=True, allow_redirects=True, verify=False, timeout=3).ok
 
 def extract(session, url):
-    with session.post(ANIME1 + "content/episode/", verify=False, headers={'referer': url, 'x-requested-with': 'XMLHttpRequest'}) as content_access:
-        cooked_cookie = content_access.headers.get('set-cookie', '')
-    
     healthy = False
-
     while not healthy:        
-        with session.get(url, verify=False, headers={'cookie': cooked_cookie}) as episode:
+        with session.get(url, verify=False) as episode:
             stream_url = STREAM_EXTRACTOR_RE.search(episode.text).group(0)
-        cooked_cookie = episode.headers.get('set-cookie', '')
         healthy = is_healthy(session, stream_url)
-        
     return {'quality': 'unknown', 'stream_url': stream_url}
-
 
 def fetcher(session, url, check):
     match = EPISODE_RE.search(url)
