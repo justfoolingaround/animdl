993f7bf88b17ebe10fd7d1ac855fb96576b6613b
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-13 09:57:42 +0545

    [Mp4Upload] Add: Support for 480p~ extraction

--- core/providers/nineanime/inner/mp4upload.py
+++ core/providers/nineanime/inner/mp4upload.py
@@ -8,6 +8,12 @@ def uri_correction(mp4upload_uri):
     """
     return "https://www.mp4upload.com/embed-%s.html" % re.search(r"embed-(.*)\.html", mp4upload_uri).group(1)
 
+def extract_480(splitted_values):
+    return {'quality': splitted_values[50], 'stream_url': "{4}://{18}.{3}.{1}:{70}/d/{69}/{68}.{67}".format(*splitted_values)}
+
+def extract_any(splitted_values):
+    return {'quality': splitted_values[53], 'stream_url': "{3}://{18}.{1}.{0}:{73}/d/{72}/{71}.{70}".format(*splitted_values)}    
+
 def extract(session, mp4upload_uri):
     """
     A curated-random extraction for MP4Upload.
@@ -17,7 +23,8 @@ def extract(session, mp4upload_uri):
         if mp4upload_embed_page.text == 'File was deleted':
             return []
         content = MP4UPLOAD_REGEX.search(mp4upload_embed_page.text).group(1).split('|')
+
         try:
-            return [{'quality': content[53], 'stream_url': "{3}://{18}.{1}.{0}:{73}/d/{72}/{71}.{70}".format(*content), 'headers': {'referer': mp4upload_uri, 'ssl_verification': False}}]
+            return [{**(extract_480 if '480' in content else extract_any)(content), 'headers': {'referer': mp4upload_uri, 'ssl_verification': False}}]
         except Exception as e:
-            raise Exception("Exception '%s' on Mp4Upload url: %s" % (e, mp4upload_uri))
+            raise Exception("'%s' occurred when extracting from '%s'." % (e, mp4upload_uri))
