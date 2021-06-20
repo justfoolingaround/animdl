30b270589b85a5787e9c6acbadf8603995233607
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-20 10:12:31 +0545

    Feat: Made config more configurable

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -1,29 +1,60 @@
-"""
-Change stuff here if the site got changed / banned in your region.
-"""
+import json
+import os
+from pathlib import Path
 
-FOURANIME = "https://4anime.to/"
-NINEANIME = "https://9anime.to/"
-ANIME1 = "http://www.anime1.com/"
-ANIMEFREAK = "https://animefreak.tv/"
-ANIMEPAHE = "https://animepahe.com/"
-ANIMIXPLAY = "https://animixplay.to/"
-GOGOANIME = "https://gogoanime.ai/"
-TWIST = "https://twist.moe/"
 
-"""
-Set preferred quality here. (Only for HLS streams)
-"""
+def merge_dicts(dict1, dict2):
+    for k, v in dict1.items():
+        if isinstance(v, dict):
+            merge_dicts(v, dict2.setdefault(k, {}))
+        else:
+            if not k in dict2:
+                dict2[k] = v
+    return dict2
+        
+CONFIGURATION_FILE_PATH = Path(os.getenv('ANIMDL_CONFIG') or './animdl_config.json')
 
-QUALITY = 1080
+DEFAULT_CONFIG = {
+    'site_urls': {
+        '4anime': 'https://4anime.to/',
+        '9anime': 'https://9anime.to/',
+        'anime1': 'https://www.anime1.com/',
+        'animefreak': 'https://animefreak.tv/',
+        'animepahe': 'https://animepahe.com/',
+        'animixplay': 'https://animixplay.to/',
+        'gogoanime': 'https://gogoanime.ai/',   
+        'twist': 'https://twist.moe/',   
+    },
+    'preferred_quality': 1080,
+    'mpv_executable': 'mpv',
+    'schedule': {
+        'site_url': 'https://www.livechart.me/',
+        'date_format': '%b. %d, %A',
+        'time_format': '%X'
+    }
+}
 
-MPV_EXECUTABLE = "mpv"
+CONFIG = DEFAULT_CONFIG
 
-"""
-Below is for schedule:
-"""
+if CONFIGURATION_FILE_PATH.exists():
+    with open(CONFIGURATION_FILE_PATH, 'r') as conf:
+        CONFIG = merge_dicts(DEFAULT_CONFIG, json.load(conf))
+        
+SITE_URLS = CONFIG.get('site_urls', {})
 
-LIVECHART = "https://www.livechart.me/"
+FOURANIME  = SITE_URLS.get('4anime')
+NINEANIME  = SITE_URLS.get('9anime')
+ANIME1     = SITE_URLS.get('anime1')
+ANIMEFREAK = SITE_URLS.get('animefreak')
+ANIMEPAHE  = SITE_URLS.get('animepahe')
+ANIMIXPLAY = SITE_URLS.get('animixplay')
+GOGOANIME  = SITE_URLS.get('gogoanime')
+TWIST      = SITE_URLS.get('twist')
 
-DATE_FORMAT = "%b. %d, %A"
-TIME_FORMAT = "%X"
+QUALITY        = CONFIG.get('preferred_quality')
+MPV_EXECUTABLE = CONFIG.get('mpv_executable')
+
+LIVECHART   = CONFIG.get('schedule', {}).get('site_url')
+
+DATE_FORMAT = CONFIG.get('schedule', {}).get('date_format')
+TIME_FORMAT = CONFIG.get('schedule', {}).get('time_format')
