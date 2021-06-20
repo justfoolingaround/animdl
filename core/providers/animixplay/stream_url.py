389697da9792a3d80eb5dd4f2320710f2102d1c8
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-20 12:54:16 +0545

    [Animix] Fix: New GGA content policy (#11)

--- core/providers/animixplay/stream_url.py
+++ core/providers/animixplay/stream_url.py
@@ -8,6 +8,10 @@ import lxml.html as htmlparser
 import json
 import re
 
+ajax_parse = lambda dt: (
+    dt.get('source', [{}])[0].get('file'), dt.get('source', [{}])[0].get('label'), dt.get('source', [{}])[0].get('type'), 
+    dt.get('source_bk', [{}])[0].get('file'), dt.get('source_bk', [{}])[0].get('label'), dt.get('source_bk', [{}])[0].get('type'))
+
 def fetching_chain(f1, f2, session, url, check=lambda *args: True):
     return f2(session, f1(session, url), check=check)
 
@@ -17,9 +21,22 @@ def from_site_url(session, url) -> dict:
     """
     return json.loads(htmlparser.fromstring(session.get(url).content).xpath('//div[@id="epslistplace"]')[0].text)
 
-def gogoanime_parser(session, data: dict, *, check=lambda *args: True):
+def bypass_encrypted_content(session, streaming_url):
+    with session.get('https:%s' % streaming_url.replace('streaming', 'loadserver')) as server_load:
+        for urls in re.finditer(r"(?<=sources:\[{file: ')[^']+", server_load.text):
+            yield urls.group(0)
+
+def get_stream_url(session, data_url):
+    with session.get('https:%s' % data_url.replace('streaming', 'ajax')) as response:
+        content = response.json()
     
-    ajax_parse = lambda dt: [{'quality': 'unknown', 'stream_url': dt.get('source', [{}])[0].get('file')}, {'quality': 'unknown', 'stream_url': dt.get('source_bk', [{}])[0].get('file')}]
+    if content == 404:
+        return [{'quality': 'unknown', 'stream_url': c, 'headers': {'referer': "https:%s" % data_url}} for c in bypass_encrypted_content(session, data_url)]
+        
+    s1, l1, t1, s2, l2, t2 =  ajax_parse(content)
+    return [{'quality': "%s [%s]" % (l1, t1), 'stream_url': s1}] + ([{'quality': "%s [%s]" % (l2, t2), 'stream_url': s2}] if s2 else [])
+
+def gogoanime_parser(session, data: dict, *, check=lambda *args: True):
     for value in range(data.get('eptotal')):
         if check(value + 1):
-            yield partial(lambda session, data, value: (ajax_parse(session.get("https:%s" % data[str(value)].replace('streaming', 'ajax')).json())), session, data, value), value + 1
+            yield partial(lambda s, data_url: get_stream_url(s, data_url), session, data[str(value)]), value + 1
