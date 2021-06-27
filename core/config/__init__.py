a5df5d288a1fe6a7ed8d2567949f49e29a197526
Justaus3r <72691864+Justaus3r@users.noreply.github.com>
2021-06-28 00:37:38 +0500

    Feat:Add vlc anime streaming

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -1,7 +1,7 @@
 import json
 import os
 from pathlib import Path
-
+import shutil
 
 def merge_dicts(dict1, dict2):
     for k, v in dict1.items():
@@ -13,6 +13,13 @@ def merge_dicts(dict1, dict2):
     return dict2
         
 CONFIGURATION_FILE_PATH = Path(os.getenv('ANIMDL_CONFIG') or './animdl_config.json')
+
+if bool(shutil.which('mpv')):
+    player_executable = 'mpv'
+elif bool(shutil.which('vlc')):
+    player_executable = 'vlc'
+else:
+    player_executable = '_never gonna give you up,never gonna let you down_' # GARBAGE VALUE,Since i had to assign a string,then why not rickroll everyone 
 
 DEFAULT_CONFIG = {
     'session_file': 'cli_session_animdl.json',
@@ -29,13 +36,15 @@ DEFAULT_CONFIG = {
         'twist': 'https://twist.moe/',
     },
     'preferred_quality': 1080,
-    'mpv_executable': 'mpv',
+    'player_executable':f"{player_executable}",
     'schedule': {
         'site_url': 'https://www.livechart.me/',
         'date_format': '%b. %d, %A',
         'time_format': '%X'
     }
 }
+
+
 
 CONFIG = DEFAULT_CONFIG
 
@@ -56,7 +65,9 @@ GOGOANIME  = SITE_URLS.get('gogoanime')
 TWIST      = SITE_URLS.get('twist')
 
 QUALITY        = CONFIG.get('preferred_quality')
-MPV_EXECUTABLE = CONFIG.get('mpv_executable')
+
+PLAYER_EXECUTABLE = CONFIG.get('player_executable')
+
 
 LIVECHART   = CONFIG.get('schedule', {}).get('site_url')
 
