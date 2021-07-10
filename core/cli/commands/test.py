afaa0f91f074749cc1ffbc3597c626b8ab7076cb
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-07-10 15:12:14 +0545

    Fix: Accidental global call

--- core/cli/commands/test.py
+++ core/cli/commands/test.py
@@ -4,23 +4,23 @@ import requests
 from ...codebase import Associator
 from ..helpers import to_stdout, bannerify
 
-SITE_LIST = {
-    '4anime': 'https://4anime.to/anime/one-piece',
-    '9anime': 'https://9anime.to/watch/one-piece.ov8',
-    'anime1': 'https://www.anime1.com/watch/one-piece',
-    'animepahe': requests.get('https://pahe.win/a/4', allow_redirects=False).headers.get('location', ''),
-    'animeout': 'https://www.animeout.xyz/download-one-piece-episodes-latest/',
-    'animixplay': 'https://animixplay.to/v1/one-piece',
-    'gogoanime': 'https://gogoanime.ai/category/one-piece',
-    'twist': 'https://twist.moe/a/one-piece',
-}
-
 @click.command(name='test', help="Test the scrapability power.")
 @click.option('-x', help='A list of certain sites (full anime url) to be explicity used for testing.', default=[], required=False, multiple=True)
 @click.option('-e', help='Episode number to bestow the testing upon', default=1, required=False, type=int)
 @click.option('--quiet', help='A flag to silence all the announcements.', is_flag=True, flag_value=True)
 @bannerify
 def animdl_test(x, e, quiet):
+    SITE_LIST = {
+        '4anime': 'https://4anime.to/anime/one-piece',
+        '9anime': 'https://9anime.to/watch/one-piece.ov8',
+        'anime1': 'https://www.anime1.com/watch/one-piece',
+        'animepahe': requests.get('https://pahe.win/a/4', allow_redirects=False).headers.get('location', ''),
+        'animeout': 'https://www.animeout.xyz/download-one-piece-episodes-latest/',
+        'animixplay': 'https://animixplay.to/v1/one-piece',
+        'gogoanime': 'https://gogoanime.ai/category/one-piece',
+        'twist': 'https://twist.moe/a/one-piece',
+    }
+
     if not x:
         x = SITE_LIST.values()
         
