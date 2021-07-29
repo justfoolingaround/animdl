1e5c57eaf99312f50af59b847160addca81bc58a
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-07-29 13:48:25 +0545

    [Animix] Fix: Some streams undetected

--- core/codebase/providers/animixplay/stream_url.py
+++ core/codebase/providers/animixplay/stream_url.py
@@ -8,7 +8,7 @@ import lxml.html as htmlparser
 ID_MATCHER = re.compile(r"(?<=\?id=)[^&]+")
 
 EMBED_URL_BASE = "https://animixplay.to/api/live{}"
-EMBED_M3U8_MATCHER = re.compile(r'(?<=player\.html#)[^#]+')
+EMBED_M3U8_MATCHER = re.compile(r'(?<=player\.html[?#])[^#]+')
 EMBED_VIDEO_MATCHER = re.compile(r'(?<=video=")[^"]+')
 
 
@@ -28,29 +28,38 @@ def from_site_url(session, url) -> dict:
                     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.166 Safari/537.36'}).content).xpath('//div[@id="epslistplace"]')[0].text)
 
 
-def get_stream_url(session, data_url):
-    content_id = ID_MATCHER.search(data_url).group(0).encode(errors='ignore')
-    while True:
+def get_embed(session, data_url):
+    content_id_re = ID_MATCHER.search(data_url)
+    if not content_id_re:
+        return data_url
+    content_id = content_id_re.group(0).encode(errors='ignore')
+
+    resp = 0
+
+    while resp != 200:
         embed_page = session.get(
-            EMBED_URL_BASE.format(
-                b64encode(
-                    b"%sLTXs3GrU8we9O%s" %
-                    (content_id,
-                     b64encode(content_id))).decode(
-                    errors='ignore')),
-            headers={
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.166 Safari/537.36'},
-            allow_redirects=True)
-        if embed_page.status_code == 429:
-            continue
-        video_on_site = EMBED_VIDEO_MATCHER.search(embed_page.text)
+                EMBED_URL_BASE.format(
+                    b64encode(
+                        b"%sLTXs3GrU8we9O%s" %
+                        (content_id,
+                        b64encode(content_id))).decode(
+                        errors='ignore')),
+                headers={
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.166 Safari/537.36'},
+                allow_redirects=True)
+        resp = embed_page.status_code
+    return embed_page
+
+
+def get_stream_url(session, data_url):
+    url = get_embed(session, data_url)
+    if not isinstance(url, str):
+        video_on_site = EMBED_VIDEO_MATCHER.search(url.text)
         if video_on_site:
             return [{'stream_url': video_on_site.group(0)}]
-        embed_m3u8 = EMBED_M3U8_MATCHER.search(embed_page.url)
-        if not embed_m3u8:
-            continue
-        return [{'stream_url': b64decode(EMBED_M3U8_MATCHER.search(embed_page.url).group(
-            0).encode(errors='ignore')).decode(errors='ignore'), 'quality': 'multi'}]
+        url = url.url
+    return [{'stream_url': b64decode(EMBED_M3U8_MATCHER.search(url).group(
+        0).encode(errors='ignore')).decode(errors='ignore'), 'quality': 'multi'}]
 
 
 def gogoanime_parser(session, data: dict, *, check=lambda *args: True):
