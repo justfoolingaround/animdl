ab9e2081bd71f3d67f0abd6921eab275975a3c7f
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-08-22 23:46:22 +0545

    Chore: Workflows

---
+++ animdl/runner.py
@@ -0,0 +1,35 @@
+"""
+__main__ but a runner for the standalones.
+"""
+
+import click
+
+from core import logger
+from core.cli.commands import download, stream, continuation, grab, schedule, test, search
+from core.cli.helpers.player import supported_streamers
+
+
+logger.configure_logger()
+
+commands = {
+    'download': download.animdl_download,
+    'continue': continuation.animdl_continue,
+    'grab': grab.animdl_grab,
+    'schedule': schedule.animdl_schedule,
+    'test': test.animdl_test,
+    'search': search.animdl_search,
+}
+
+executable = [*supported_streamers()]
+
+if executable:
+    commands.update({'stream': stream.animdl_stream})
+
+
+@click.group(commands=commands)
+def __animdl_cli__():
+    pass
+
+
+if __name__ == '__main__':
+    __animdl_cli__()
