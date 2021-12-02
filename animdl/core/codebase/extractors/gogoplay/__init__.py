3b58d425673c0ba1fb77b97d09b82dd68f8816c5
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-12-02 20:08:29 +0545

    Fix: 403 URLs in GogoAnime extraction

--- animdl/core/codebase/extractors/gogoplay/__init__.py
+++ animdl/core/codebase/extractors/gogoplay/__init__.py
@@ -1,46 +1,24 @@
-from base64 import b64decode
-from functools import reduce
-
 import lxml.html as htmlparser
 import regex
 import yarl
-
-DOWNLOAD_URL_RE = regex.compile(r"download\.php\?url=([^?&/]+)")
-
-GARBAGES = [
-    'URASDGHUSRFSJGYfdsffsderFStewthsfSFtrfte',
-    'AdeqwrwedffryretgsdFrsftrsvfsfsr',
-    'werFrefdsfrersfdsrfer36343534',
-    'AawehyfcghysfdsDGDYdgdsf',
-    'wstdgdsgtert',
-    'Adrefsd',
-    'sdf',
-]
-
-def decrypt_redirect(url):
-    is_gogocdn = DOWNLOAD_URL_RE.search(url)
-    
-    if not is_gogocdn:
-        return url
-
-    encrypted_url = reduce(lambda x, y: x.replace(y, ''), GARBAGES, is_gogocdn.group(1))
-    
-    return b64decode(encrypted_url + "="*(len(encrypted_url) % 4)).decode()
 
 
 def get_quality(url_text):
     match = regex.search(r'(\d+)P', url_text)
     if not match:
         return None
+
     return int(match.group(1))
 
-
 def extract(session, url, **opts):
+    """
+    Extract content off of GogoAnime.
+    """
     parsed_url = yarl.URL(url)
 
     if not parsed_url.scheme:
         parsed_url = parsed_url.with_scheme('https')
-
+    
     content_url = parsed_url.with_name('download').with_query(parsed_url.query).human_repr()
 
     response = session.get(
@@ -49,4 +27,4 @@ def extract(session, url, **opts):
             'referer': url})
     content = htmlparser.fromstring(response.text)
 
-    return [{'quality': get_quality(url.text_content()), 'stream_url': decrypt_redirect(url.get('href')), 'headers': {'referer': url.get('href')}} for url in content.cssselect('.dowload > a[download]')]
+    return [{'quality': get_quality(url.text_content()), 'stream_url': url.get('href'), 'headers': {'referer': url}} for url in content.cssselect('.dowload > a[download]')]
