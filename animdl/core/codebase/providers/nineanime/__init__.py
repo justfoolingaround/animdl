871e75975c046cf80318c215b17eb1e4804bc69f
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-12-08 21:39:19 +0545

    Chore: 9Anime cleanup

--- animdl/core/codebase/providers/nineanime/__init__.py
+++ animdl/core/codebase/providers/nineanime/__init__.py
@@ -1,12 +1,3 @@
-"""
-Probably the only 9Anime scraper in the world that does not rely on Selenium or webdrivers to scrape.
-"""
-
-import json
-from functools import partial
-
-import lxml.html as htmlparser
-
 from ....config import NINEANIME
 from ...helper import construct_site_based_regex
 
