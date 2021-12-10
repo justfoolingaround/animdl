420fdc1bd39d237be7ee695f0d98928567d01349
justfoolingaround <kr.justfoolingaround@gmail.com>
2021-12-10 14:57:48 +0545

    Feat: Added quality in StreamSB extractor

--- animdl/core/codebase/extractors/streamsb/__init__.py
+++ animdl/core/codebase/extractors/streamsb/__init__.py
@@ -1,21 +1,32 @@
-import re
 import lxml.html as htmlparser
+import regex
 
-DOWNLOAD_CONTENT = re.compile(r"download_video\('(.+?)','(.+?)','(.+?)'\)")
+DOWNLOAD_CONTENT = regex.compile(r"download_video\('(.+?)','(.+?)','(.+?)'\)")
+QUALITY_RE = regex.compile(r"\d+x(\d+)")
 
 def extract(session, url, **opts):
     content = htmlparser.fromstring(session.get(url).text)
 
     def fast_yield():
-        for links in content.cssselect('a[onclick^="download_video"]'):
-            content_id, mode, content_hash = DOWNLOAD_CONTENT.search(links.get('onclick')).groups()
+        genexp = iter(content.cssselect('tr > td'))
+        for link_column, quality_column in zip(genexp, genexp):
+            
+            links = link_column.cssselect('a')
+
+            if not links:
+                continue
+            
+            quality_match = QUALITY_RE.search(quality_column.text_content())
+            quality = int(quality_match.group(1)) if quality_match else None
+
+            content_id, mode, content_hash = DOWNLOAD_CONTENT.search(links[0].get('onclick')).groups()
 
             download_link = False
             while not download_link:
-                download_page = htmlparser.fromstring(session.get('https://sbplay.org/dl', params={'op': 'download_orig', 'id': content_id, 'mode': mode, 'hash': content_hash}, headers={'referer': url}).text).cssselect('a[href$="mp4"]')
+                download_page = htmlparser.fromstring(session.get('https://sbplay1.com/dl', params={'op': 'download_orig', 'id': content_id, 'mode': mode, 'hash': content_hash}, headers={'referer': url}).text).cssselect('a[href$="mp4"]')
 
                 for direct_download in download_page:
-                    yield {'stream_url': direct_download.get('href'), 'headers': {'referer': url}}
+                    yield {'stream_url': direct_download.get('href'), 'headers': {'referer': url}, 'quality': quality}
                     download_link = True
     
     return list(fast_yield())
