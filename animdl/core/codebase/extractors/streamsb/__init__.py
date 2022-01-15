b0f5c06876d2d4bb128d4acc670ae3535bc313d7
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-01-15 19:18:52 +0545

    Fix: StreamSB extraction not working

--- animdl/core/codebase/extractors/streamsb/__init__.py
+++ animdl/core/codebase/extractors/streamsb/__init__.py
@@ -1,50 +1,19 @@
-import lxml.html as htmlparser
+import binascii
+
 import regex
+import yarl
 
-DOWNLOAD_CONTENT = regex.compile(r"download_video\('(.+?)','(.+?)','(.+?)'\)")
-QUALITY_RE = regex.compile(r"\d+x(\d+)")
-
+PAYLOAD = "616e696d646c616e696d646c7c7c{}7c7c616e696d646c616e696d646c7c7c73747265616d7362/616e696d646c616e696d646c7c7c363136653639366436343663363136653639366436343663376337633631366536393664363436633631366536393664363436633763376336313665363936643634366336313665363936643634366337633763373337343732363536313664373336327c7c616e696d646c616e696d646c7c7c73747265616d7362"
+CONTENT_ID_REGEX = regex.compile(r"/e/([^.]+)")
 
 def extract(session, url, **opts):
-    content = htmlparser.fromstring(session.get(url.replace("/e/", "/d/")).text)
 
-    def fast_yield():
-        genexp = iter(content.cssselect("tr > td"))
-        for link_column, quality_column in zip(genexp, genexp):
+    content_id = CONTENT_ID_REGEX.search(url).group(1)
+    content_url = "https://{}/".format(yarl.URL(url).host)
 
-            links = link_column.cssselect("a")
+    sources = session.get(content_url + "sourcessx36/{}".format(PAYLOAD.format(binascii.hexlify(content_id.encode()).decode())), headers={'watchsb': 'streamsb'}).json().get('stream_data', {})
 
-            if not links:
-                continue
-
-            quality_match = QUALITY_RE.search(quality_column.text_content())
-            quality = int(quality_match.group(1)) if quality_match else None
-
-            content_id, mode, content_hash = DOWNLOAD_CONTENT.search(
-                links[0].get("onclick")
-            ).groups()
-
-            download_link = False
-            while not download_link:
-                download_page = htmlparser.fromstring(
-                    session.get(
-                        "https://sbplay1.com/dl",
-                        params={
-                            "op": "download_orig",
-                            "id": content_id,
-                            "mode": mode,
-                            "hash": content_hash,
-                        },
-                        headers={"referer": url},
-                    ).text
-                ).cssselect('a[href$="mp4"]')
-
-                for direct_download in download_page:
-                    yield {
-                        "stream_url": direct_download.get("href"),
-                        "headers": {"referer": url},
-                        "quality": quality,
-                    }
-                    download_link = True
-
-    return list(fast_yield())
+    return [
+        {'stream_url': sources.get('file'), 'headers': {'referer': content_url}},
+        {'stream_url': sources.get('backup'), 'headers': {'referer': content_url}}
+    ]
