88e6853ab1fbfa9f5a32b28e1f0444ba43ec9e38
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-04-18 16:47:56 +0545

    (Auto) Fix: GogoAnime keys

--- animdl/core/codebase/extractors/gogoplay/__init__.py
+++ animdl/core/codebase/extractors/gogoplay/__init__.py
@@ -5,10 +5,11 @@ import functools
 import regex
 import yarl
 from Cryptodome.Cipher import AES
-from binascii import hexlify
+from binascii import unhexlify
 
 CUSTOM_PADDER = "\x08\x0e\x03\x08\t\x03\x04\t"
 ENCRYPTION_KEYS = "https://raw.githubusercontent.com/justfoolingaround/animdl-provider-benchmarks/master/api/gogoanime.json"
+
 
 def get_quality(url_text):
     match = regex.search(r"(\d+) P", url_text)
@@ -25,20 +26,20 @@ def pad(data):
 
 def aes_encrypt(data: str, *, key, iv):
     return base64.b64encode(
-        AES.new(key, AES.MODE_CBC, iv=iv).encrypt(
-            pad(data).encode()
-        )
+        AES.new(key, AES.MODE_CBC, iv=iv).encrypt(pad(data).encode())
     )
 
 
 def aes_decrypt(data: str, *, key, iv):
-    return AES.new(key, AES.MODE_CBC, iv=iv).decrypt(
-        base64.b64decode(data)
-    )
+    return AES.new(key, AES.MODE_CBC, iv=iv).decrypt(base64.b64decode(data))
+
 
 @functools.lru_cache()
 def get_encryption_keys(session):
-    return {_: __.encode() for _, __ in session.get(ENCRYPTION_KEYS).json().items()}
+    return {
+        _: unhexlify(__.encode())
+        for _, __ in session.get(ENCRYPTION_KEYS).json().items()
+    }
 
 
 def extract(session, url, **opts):
@@ -59,18 +60,19 @@ def extract(session, url, **opts):
 
     keys = get_encryption_keys(session)
     content_id = parsed_url.query.get("id")
-    
-    key = hexlify(base64.b64decode(content_id) + keys["iv"])[:32]
 
     response = session.get(
         "{}encrypt-ajax.php".format(next_host),
-        params={"id": aes_encrypt(content_id, key=key, iv=keys["iv"]).decode(), "alias": content_id},
+        params={
+            "id": aes_encrypt(content_id, key=keys["key"], iv=keys["iv"]).decode(),
+            "alias": content_id,
+        },
         headers={"x-requested-with": "XMLHttpRequest", "referer": next_host},
     )
     content = json.loads(
-        aes_decrypt(response.json().get("data"), key=key, iv=keys["iv"]).strip(
-            b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10"
-        )
+        aes_decrypt(
+            response.json().get("data"), key=keys["second_key"], iv=keys["iv"]
+        ).strip(b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10")
     )
 
     def yielder():
