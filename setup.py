fbd0735ba0632042d3d785beec3f95ab8fc666e4
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-06-06 00:16:58 +0545

    Chore: Make `requirements.txt` be used

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 from animdl.core.__version__ import __core__
 
+with open("./requirements.txt") as requirements_txt:
+    requirements = requirements_txt.read().splitlines()
+
+
 setup(
     name="animdl",
     version=__core__,
@@ -11,31 +15,11 @@ setup(
     url="https://github.com/justfoolingaround/animdl",
     keywords=[
         "stream",
+        "cli",
         "anime",
         "download",
-        "anime-downloader",
-        "twist",
-        "9anime",
-        "gogoanime",
-        "animepahe",
-        "4anime",
-        "anime-streamer",
-        "fouranime",
-        "animixplay",
     ],
-    install_requires=[
-        "anitopy==2.1.0",
-        "click==8.0.4",
-        "comtypes==1.1.11",
-        "cssselect==1.1.0",
-        "httpx==0.23.0",
-        "lxml==4.8.0",
-        "tqdm==4.62.3",
-        "pycryptodomex==3.14.1",
-        "regex==2022.3.15",
-        "yarl==1.7.2",
-        "pyyaml==6.0",
-    ],
+    install_requires=requirements,
     entry_points="""
         [console_scripts]
         animdl=animdl.__main__:__animdl_cli__
