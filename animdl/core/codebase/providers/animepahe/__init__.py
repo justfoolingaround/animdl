962d95a73c40db50cac8b998ad56f0432803cfab
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-06-20 21:04:52 +0545

    Fix: Animepahe urls

--- animdl/core/codebase/providers/animepahe/__init__.py
+++ animdl/core/codebase/providers/animepahe/__init__.py
@@ -2,7 +2,6 @@ import regex
 
 from ....config import ANIMEPAHE
 from ...helper import construct_site_based_regex
-from .inner import get_stream_url_from_kwik
 
 from functools import partial, lru_cache
 
@@ -14,7 +13,7 @@ SITE_URL = ANIMEPAHE
 
 PLAYER_RE = construct_site_based_regex(ANIMEPAHE, extra_regex=r"/play/([^?&/]+)")
 ID_RE = regex.compile(r'let id = "(.+?)"')
-KWIK_RE = regex.compile(r"Plyr\|querySelector\|document\|([^\\']+)")
+KWIK_RE = regex.compile(r"Plyr\|(.+?)'")
 
 TITLES_REGEX = regex.compile(r"<h1>(.+?)</h1>")
 
@@ -27,18 +26,10 @@ def get_session_page(session, page, release_id):
     ).json()
 
 
-def get_m3u8_from_kwik(session, kwik_url):
-    """
-    (Unused at the moment!)
-    """
-    kwik_page = session.get(kwik_url, headers={"referer": SITE_URL})
-    match = KWIK_RE.search(kwik_page.text)
-    if match:
-        return "{10}://{9}-{8}.{7}.{6}.{5}/{4}/{3}/{2}/{1}.{0}".format(
-            *match.group(1).split("|")
-        )
-    raise Exception(
-        "Session fetch failure; please recheck and/or retry fetching anime URLs again. If this problem persists, please make an issue immediately."
+def get_streams_from_embed_url(session, embed_uri):
+    embed_page = session.get(embed_uri, headers={"referer": ANIMEPAHE}).text
+    return "{}://{}-{}.{}.{}.{}/{}/{}/{}/{}.{}".format(
+        *KWIK_RE.search(embed_page).group(1).split("|")[::-1]
     )
 
 
@@ -52,10 +43,11 @@ def get_stream_url(session, release_id, stream_session):
 
     for d in content:
         for quality, data in d.items():
+
             yield {
                 "quality": quality,
                 "headers": {"referer": data.get("kwik")},
-                "stream_url": get_m3u8_from_kwik(session, data.get("kwik")),
+                "stream_url": get_streams_from_embed_url(session, data.get("kwik")),
             }
 
 
