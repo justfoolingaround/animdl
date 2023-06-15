24f4f28d912a9176dfba33066bc2096cf5f26cba
justfoolingaround <kr.justfoolingaround@gmail.com>
2023-06-16 00:50:50 +0545

    Chore: Bump version to `1.7.16`

--- animdl/core/__version__.py
+++ animdl/core/__version__.py
@@ -2,6 +2,6 @@ import importlib.metadata
 
 # Let this be a temporary fix for now as this ensures older versions remain updated.
 
-__core__ = "1.7.15"
+__core__ = "1.7.16"
 
 __core__ = importlib.metadata.version("animdl")
