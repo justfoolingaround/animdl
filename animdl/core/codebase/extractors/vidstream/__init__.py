8c84ac48235aba0584b688f162fc6baea8ec1f48
justfoolingaround <kr.justfoolingaround@gmail.com>
2023-07-25 09:37:18 +0545

    Fix: Schrodinger's error on `black`'s end

--- animdl/core/codebase/extractors/vidstream/__init__.py
+++ animdl/core/codebase/extractors/vidstream/__init__.py
@@ -10,13 +10,12 @@ CHAR_SUBST_OFFSETS = [-6, 3, 3, -5, 2, -6]
 def extract(session, url, **opts):
     match = EMBED_URL_REGEX.search(url)
     host = match.group(1)
-    video_id = match.group(2)
-
+    video_id = generate_vrf_from_content_id(
+        match.group(2), offsets=CHAR_SUBST_OFFSETS, key=ID_KEY, reverse_later=False
+    )
 
     vidstream_info = session.get(
-        f"{host}/mediainfo/{generate_vrf_from_content_id(
-            video_id, offsets=CHAR_SUBST_OFFSETS, key=ID_KEY, reverse_later=False
-        )}", headers={"referer": url}
+        f"{host}/mediainfo/{video_id}", headers={"referer": url}
     )
 
     return [
