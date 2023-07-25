8e1ed234407ac33807b162713a0c0d21d12ffb64
uh wot <uhwot@protonmail.com>
2023-07-25 04:16:01 +0200

    Avoid redefining builtin functions

--- animdl/core/codebase/extractors/vidstream/__init__.py
+++ animdl/core/codebase/extractors/vidstream/__init__.py
@@ -12,22 +12,22 @@ EMBED_URL_REGEX = regex.compile(r"(.+?)/(?:e(?:mbed)?)/([a-zA-Z0-9]+)")
 ID_KEY = b"FtFyeHeWL36bANDy"
 CHAR_SUBST_OFFSETS = [-6, 3, 3, -5, 2, -6]
 
-def mediainfo_id(id):
-    id = quote(id)
-    id = ARC4.new(ID_KEY).encrypt(id.encode())
-    id = b64encode(id).decode()
-    id = id[::-1]
-    id = char_subst(id, CHAR_SUBST_OFFSETS)
-    id = b64encode(id.encode()).decode()
-    return id
+def mediainfo_id(vid_id):
+    result = quote(vid_id)
+    result = ARC4.new(ID_KEY).encrypt(result.encode())
+    result = b64encode(result).decode()
+    result = result[::-1]
+    result = char_subst(result, CHAR_SUBST_OFFSETS)
+    result = b64encode(result.encode()).decode()
+    return result
 
 def extract(session, url, **opts):
     match = EMBED_URL_REGEX.search(url)
     host = match.group(1)
-    id = match.group(2)
+    vid_id = match.group(2)
 
     vidstream_info = session.get(
-        f"{host}/mediainfo/{mediainfo_id(id)}",
+        f"{host}/mediainfo/{mediainfo_id(vid_id)}",
         headers={'referer': url}
     )
 
--- animdl/core/codebase/providers/nineanime/decipher.py
+++ animdl/core/codebase/providers/nineanime/decipher.py
@@ -6,19 +6,19 @@ VRF_KEY = b"iECwVsmW38Qe94KN"
 URL_KEY = b"hlPeNwkncH0fq9so"
 CHAR_SUBST_OFFSETS = [-4, -4, 3, 3, 6, -4, 3, -6, -2, -4]
 
-def vrf_gen(id):
-    vrf = quote(id)
-    vrf = ARC4.new(VRF_KEY).encrypt(id.encode())
+def vrf_gen(vid_id):
+    vrf = quote(vid_id)
+    vrf = ARC4.new(VRF_KEY).encrypt(vid_id.encode())
     vrf = base64.b64encode(vrf).decode()
     vrf = char_subst(vrf, CHAR_SUBST_OFFSETS)
     vrf = vrf[::-1]
     vrf = base64.b64encode(vrf.encode()).decode()
     return vrf
 
-def char_subst(str, offsets):
+def char_subst(string, offsets):
     result = ""
-    for i in range(len(str)):
-        result += chr(ord(str[i]) + offsets[i % len(offsets)])
+    for i in range(len(string)):
+        result += chr(ord(string[i]) + offsets[i % len(offsets)])
     return result
 
 def decrypt_url(encrypted_url):
