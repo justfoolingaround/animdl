59cb47f9aceb34da22cf510b34def3103d3b1df9
justfoolingaround <kr.justfoolingaround@gmail.com>
2023-08-01 13:36:41 +0545

    Chore: Change `password` to `pwd_from_url`

--- animdl/core/codebase/extractors/rapidvideo/utils.py
+++ animdl/core/codebase/extractors/rapidvideo/utils.py
@@ -17,19 +17,18 @@ def pkcs5_bytes_to_key(salt: bytes, secret, *, output=48, hash_module=md5):
 
 
 def decipher_salted_aes(encoded_url: str, key_finders, *, aes_mode=AES.MODE_CBC):
-    password = ""
-
+    pwd_from_url = ""
     untouched_url = encoded_url
 
     for start, end in key_finders:
-        password += untouched_url[start:end]
+        pwd_from_url += untouched_url[start:end]
         encoded_url = encoded_url.replace(untouched_url[start:end], "")
 
     raw_value = b64decode(encoded_url.encode("utf-8"))
 
     assert raw_value.startswith(b"Salted__"), "Not a salt."
 
-    key = pkcs5_bytes_to_key(raw_value[8:16], password.encode("utf-8"))
+    key = pkcs5_bytes_to_key(raw_value[8:16], pwd_from_url.encode("utf-8"))
     return (
         unpad(AES.new(key[:32], aes_mode, key[32:]).decrypt(raw_value[16:]), 16)
         .decode("utf-8", "ignore")
